522bce2cd5e03d90fdf98c9e2a4bc2be4e1053f0
hasanhaider-dev <s.hasanhdr@gmail.com>
2021-03-30 15:48:09 +0500

    My first commit

---
+++ src/main/java/com/example/designpatterns/DesignPatterns.java
@@ -0,0 +1,13 @@
+package com.example.designpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatterns {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatterns.class, args);
+	}
+
+}
